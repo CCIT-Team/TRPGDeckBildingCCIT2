78bd71be25ad7dd0f27557cd692a3c5722af9932
MoccaHazel <wltn1937@gmail.com>
2023-09-04 17:53:47 +0900

    더미스크립트 교체, 배틀신 생성

--- Assets/Script/Battle/BattleManager.cs
+++ Assets/Script/Battle/BattleManager.cs
@@ -6,21 +6,21 @@ using UnityEngine.UI;
 public class BattleManager : MonoBehaviour
 {
     public static BattleManager instance;
-    //public List<CharacterDummy> characters;  //주석 달린부분 CharacterDummy class 지워서 생기는 부분 수정 필요
-    //CharacterDummy currentCharacter;
+    public List<Character> characters;  //주석 달린부분 Character class 지워서 생기는 부분 수정 필요
+    Character currentCharacter;
 
     bool isBattle = false;
-    //public bool Battle
-    //{
-    //    get { return isBattle; }
-    //    set
-    //    {
-    //        if (value = isBattle)
-    //            StartBattle();
-    //        else
-    //            EndBattle();
-    //    }
-    //}
+    public bool Battle
+    {
+        get { return isBattle; }
+        set
+        {
+            if (value = isBattle)
+                StartBattle();
+            else
+                EndBattle();
+        }
+    }
 
     void Awake()
     {
@@ -34,97 +34,88 @@ public class BattleManager : MonoBehaviour
     }
 
     public Button EndTurnBtn;
-    
-
-    //void Start()
-    //{
-    //    if(EndTurnBtn != null)
-    //        EndTurnBtn.onClick.AddListener(()=>ChangeTurn());
-    //}
 
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.Space))
-    //        ChangeTurn();
+    void Start()
+    {
+        if (EndTurnBtn != null)
+            EndTurnBtn.onClick.AddListener(() => ChangeTurn());
+    }
 
-    //    if (Input.GetKeyDown(KeyCode.Return))
-    //        SortSpeed();
 
-    //    if (Input.GetKeyDown(KeyCode.KeypadEnter))
-    //        print("현재 캐릭터 : " + currentCharacter.name);
-        
-    //}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            ChangeTurn();
 
-    //void StartBattle()
-    //{
-    //    //characters.Add();
-    //    SortSpeed();
-    //    currentCharacter = characters[0];
-    //}
+        if (Input.GetKeyDown(KeyCode.Return))
+            SortSpeed();
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            print("현재 캐릭터 : " + currentCharacter.name);
+    }
+
+    void StartBattle()
+    {
+        //characters.Add();
+        SortSpeed();
+        currentCharacter = characters[0];
+    }
 
     void EndBattle()
     {
         
     }
 
-    //void JoinBattle(CharacterDummy joinCharacter)
-    //{
-    //    for(int i = 0; i<= characters.Count;i++)
-    //    {
-    //        if(characters[i].SPD < joinCharacter.SPD)
-    //        {
-    //            characters.Add(joinCharacter);
-    //            return;
-    //        }
-    //    } 
-    //}
+    public void JoinBattle(Character character)     //소환, 부활 등 캐릭터 난입용
+    {
+        for (int i = 0; i <= characters.Count; i++)
+        {
+            if (characters[i].speed < character.speed)
+            {
+                characters.Add(character);
+                return;
+            }
+        }
+    }
+
+    public void ExitBattle(Character character)     //사망 등으로 인한 제거
+    {
+        characters.Remove(character);
+    }
 
 
-    //void SortSpeed()
-    //{
-    //    characters.Sort((a, b) => a.SPD > b.SPD ? 1 : -1);
-    //    foreach (CharacterDummy character in characters)
-    //        print(character.gameObject.name);
-    //}
+    void SortSpeed()    //속도에 따른 캐릭터 정렬
+    {
+        characters.Sort((a, b) => a.speed > b.speed ? 1 : -1);
+        foreach (Character character in characters)
+            print(character.gameObject.name);
+    }
 
-    //void GiveTurn()
-    //{
-    //    foreach (CharacterDummy character in characters)
-    //    {
-    //        if (character.State != State.Dead)
-    //            character.State = State.Wait;
-    //    }
-    //    characters[0].State = State.MyTurn;
-    //}
+    public void ChangeTurn(Character character, int turn = -1)      //지정된 캐릭터를 turn 턴으로 변경, turn 미지정시 가장 뒤로 변경
+    {
+        characters.Remove(character);
+        if (turn == -1)
+            characters.Insert(characters.Count, character);
+        else
+            characters.Insert(turn, character);
+        currentCharacter = characters[0];
+    }
+    public void ChangeTurn(int listNum = 0, int turn = -1)      //지정된 캐릭터를 turn 턴으로 변경,  turn 미지정시 가장 뒤로 변경, listNum 미지정시 현재 턴의 캐릭터 지정
+    {
+        if (currentCharacter == null)
+            currentCharacter = characters[listNum];
 
-    //public void ChangeTurn(CharacterDummy character, int inputNum = -1)
-    //{
-    //    //character.State = State.Wait;
-    //    characters.Remove(character);
-    //    if (inputNum == -1)
-    //        characters.Insert(characters.Count, character);
-    //    else
-    //        characters.Insert(inputNum, character);
-    //    currentCharacter = characters[0];
-    //}
-    //public void ChangeTurn(int listNum = 0, int inputNum = -1)
-    //{
-    //    //characters[listNum].State = State.Wait;
-    //    if (currentCharacter == null)
-    //        currentCharacter = characters[listNum];
-        
-    //    if (inputNum == -1)
-    //    {
-    //        characters.Remove(currentCharacter);
-    //        characters.Insert(characters.Count, currentCharacter);
-    //    }   
-    //    else
-    //    {
-    //        characters.Remove(characters[listNum]);
-    //        characters.Insert(inputNum, characters[listNum]);
-    //    }    
-    //    currentCharacter = characters[0];
-    //}
+        if (turn == -1)
+        {
+            characters.Remove(currentCharacter);
+            characters.Insert(characters.Count, currentCharacter);
+        }
+        else
+        {
+            characters.Remove(characters[listNum]);
+            characters.Insert(turn, characters[listNum]);
+        }
+        currentCharacter = characters[0];
+    }
 }
