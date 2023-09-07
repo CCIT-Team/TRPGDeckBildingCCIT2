f36f7fa2c80be58222012b31c4173e1cd8ef4dbf
tuost <fldh753@gmail.com>
2023-09-07 15:04:35 +0900

    A* 구현중

---
+++ Assets/Script/Map/Tile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    /// <summary>
+    /// Sum of G and H.
+    /// </summary>
+    public int F => g + h;
+
+    /// <summary>
+    /// Cost from start tile to this tile.
+    /// </summary>
+    public int g;
+
+    /// <summary>
+    /// Estimated cost from this tile to destination tile.
+    /// </summary>
+    public int h;
+
+    /// <summary>
+    /// Tile's coordinates.
+    /// </summary>
+    public Vector3 position;
+
+    /// <summary>
+    /// References to all adjacent tiles.
+    /// </summary>
+    public List<Tile> adjacentTiles = new List<Tile>();
+
+    /// <summary>
+    /// If true - Tile is an obstacle impossible to pass.
+    /// </summary>
+    public bool isObstacle;
+}
