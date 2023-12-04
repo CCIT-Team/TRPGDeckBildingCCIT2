24f699d3fec4fd925cd280edab48dd59f09778d0
muyoy <48443092+muyoy@users.noreply.github.com>
2023-12-04 20:19:50 +0900

    monster UI 수정

--- Assets/Script/UI/MonsterUIManager.cs
+++ Assets/Script/UI/MonsterUIManager.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class MonsterUIManager : MonoBehaviour
 {
-    private GameObject[] monster;
-
-    public GameObject selectUI;
     private void Awake()
     {
         N_BattleManager.instance.monsterUI = this;
@@ -14,11 +11,10 @@ public class MonsterUIManager : MonoBehaviour
 
     public void SetMonster(GameObject[] monster)
     {
-        //player = GameObject.FindGameObjectsWithTag("Player");
         GameObject childUI;
         for (int i = 0; i < monster.Length; i++)
         {
-            childUI = transform.GetChild(i + 1).gameObject;
+            childUI = transform.GetChild(i + 3).gameObject;
             childUI.SetActive(true);
             childUI.GetComponent<MonsterStatUI>().LinkingMonster(monster[i]);
             childUI.GetComponent<MonsterStatUI>().InitUI();
